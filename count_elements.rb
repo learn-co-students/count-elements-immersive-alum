b98a1f7d25e13717ce26646d016506fdb5082fec
pianoFF <fangfei1star@gmail.com>
2021-01-08 10:53:52 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  result = Hash.new
+  array.each do |i|
+    if result.has_key?(i)
+      result[i] += 1
+    else
+      result[i] = 1
+    end
+  end
+  result
 end
  
